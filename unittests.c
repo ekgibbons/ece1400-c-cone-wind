d198560c22b2a2910dd5bbb9ad7343499fb6bd14
Eric Gibbons <ekgibbons@gmail.com>
2023-10-12 20:00:06 -0600

    updated with swapped radius/height

--- unittests.c
+++ unittests.c
@@ -1,8 +1,11 @@
 #include "tests/utest.h"
 
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+#include "conefuncs.h"
 
 UTEST(conegeometry, usage)
 {
@@ -15,7 +18,39 @@ UTEST(conegeometry, usage)
     
 }
 
-UTEST(conegeometry, area)
+UTEST(conegeometry, area_func)
+{
+    time_t t;
+    srand((unsigned int)time(&t));
+
+    /* generate random numbers from 3 to 7 */
+    double height = ((double)rand()/(double)(RAND_MAX)) * 7 + 3;
+    double radius = ((double)rand()/(double)(RAND_MAX)) * 7 + 3; 
+
+    char command_py[200];
+    
+    sprintf(command_py,"python -c 'import tests.conegeometry "
+            "as conegeometry; "
+            "print(conegeometry.cone_area(%f, %f))' "
+            " > tmp_sol",
+            radius, height);
+
+    int ret = system(command_py);
+    ASSERT_TRUE(ret==0);
+    
+    double sub = cone_area(radius, height);
+    double sol;
+    
+    FILE *fp = fopen("tmp_sol","r");
+    fscanf(fp,"%lf", &sol);
+    fclose(fp);
+
+    ASSERT_LT(fabs(sol-sub), 1e-4);
+    ret = system("rm tmp_sol");
+    (void)ret;
+}
+
+UTEST(conegeometry, area_usage)
 {
     time_t t;
     srand((unsigned int)time(&t));
@@ -42,7 +77,39 @@ UTEST(conegeometry, area)
     (void)ret;
 }
 
-UTEST(conegeometry, volume)
+UTEST(conegeometry, volume_func)
+{
+    time_t t;
+    srand((unsigned int)time(&t));
+
+    /* generate random numbers from 3 to 7 */
+    double height = ((double)rand()/(double)(RAND_MAX)) * 7 + 3;
+    double radius = ((double)rand()/(double)(RAND_MAX)) * 7 + 3; 
+
+    char command_py[200];
+    
+    sprintf(command_py,"python -c 'import tests.conegeometry "
+            "as conegeometry; "
+            "print(conegeometry.cone_volume(%f, %f))' "
+            " > tmp_sol",
+            radius, height);
+
+    int ret = system(command_py);
+    ASSERT_TRUE(ret==0);
+    
+    double sub = cone_volume(radius, height);
+    double sol;
+    
+    FILE *fp = fopen("tmp_sol","r");
+    fscanf(fp,"%lf", &sol);
+    fclose(fp);
+
+    ASSERT_LT(fabs(sol-sub), 1e-4);
+    ret = system("rm tmp_sol");
+    (void)ret;
+}
+
+UTEST(conegeometry, volume_usage)
 {
 
     /* generate random numbers from 3 to 7 */
